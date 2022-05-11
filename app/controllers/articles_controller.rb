adefa9fce6f71a098868a7b7d860ff7731f758d3
luisaugusto <luis.augusto.u@gmail.com>
2022-05-10 23:07:30 -0500

    hemos subido ahora el new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+   @article = Article.new
+  end
+
+  def create
+  @article = Article.new(title: params[:article][:title],
+  body: params[:article][:body])
+
+
+  @article.save
+  redirect_to @article
+  end
+
 
 end
