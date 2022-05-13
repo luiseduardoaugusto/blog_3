8433691f0d1e09889e11c15fb991c31556141b7b
luisaugusto <luis.augusto.u@gmail.com>
2022-05-13 03:03:01 -0500

    hemos hecho todo CRUD ahora, antes de devise

--- Gemfile
+++ Gemfile
@@ -25,8 +25,13 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+
+gem 'awesome_print'
+
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    #where
+    #Article.where.not("id = ?",params[:id])
   end
 
   def new
@@ -13,13 +15,40 @@ class ArticlesController < ApplicationController
   end
 
   def create
-  @article = Article.new(title: params[:article][:title],
-  body: params[:article][:body])
+    @article = Article.new(article_params)
 
-
-  @article.save
-  redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 
 
 end
