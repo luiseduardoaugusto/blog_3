ddcd95d119ba2e45dd23d128c89094633020fdfc
luisaugusto <luis.augusto.u@gmail.com>
2022-05-21 17:45:59 -0500

    cambios menores resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :comments
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles, except: [:delete]
+  resources :articles, except: [:delete] do
+    resources :comments
+  end
   root "welcome#index"
 end
